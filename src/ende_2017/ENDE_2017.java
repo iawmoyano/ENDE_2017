759ddc2e19229eb6a7b7fff79d79d160aa4231b2
DanyRamos <Mc_CoY@gmx.es>
2017-03-09 14:03:03 +0100

    Update ENDE_2017.java

--- src/ende_2017/ENDE_2017.java
+++ src/ende_2017/ENDE_2017.java
@@ -5,6 +5,7 @@ public class ENDE_2017 {
     public static void main(String[] args) {
         System.out.println("Inicio proyecto ENDE 2017");
         System.out.println("Me he unido al proyecto ENDE 2017 soy  Carlos Vicente" );
+        System.out.println("DanyRamos se unio al proyecto de clase ENDE_2017");
     }
     
 }
