7e5eb58cfce0baeda747b7bac5774f84f08de19d
Ángel Miranda Raigada <ahote_satur14@hotmail.com>
2017-03-10 13:33:49 +0100

    Update ENDE_2017.java

--- src/ende_2017/ENDE_2017.java
+++ src/ende_2017/ENDE_2017.java
@@ -7,6 +7,7 @@ public class ENDE_2017 {
         System.out.println("Me he unido al proyecto ENDE 2017 soy  Carlos Vicente" );
         System.out.println("DanyRamos se unio al proyecto de clase ENDE_2017");
         System.out.println("Alberto Herrero Martin se unio al proyecto de la clase ENDE_2017");
+        System.out.println("Nino Miranda se unió al proyecto de clase ENDE_2017");
     }
     
 }
