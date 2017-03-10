19e0e3af4c81fa73eeb71a954018d9f16a5f9834
canizo <nokian95sesions@gmail.com>
2017-03-10 13:33:02 +0100

    Update ENDE_2017.java

--- src/ende_2017/ENDE_2017.java
+++ src/ende_2017/ENDE_2017.java
@@ -6,6 +6,7 @@ public class ENDE_2017 {
         System.out.println("Inicio proyecto ENDE 2017");
         System.out.println("Me he unido al proyecto ENDE 2017 soy  Carlos Vicente" );
         System.out.println("DanyRamos se unio al proyecto de clase ENDE_2017");
+        System.out.println("Alberto Herrero Martin se unio al proyecto de la clase ENDE_2017");
     }
     
 }
